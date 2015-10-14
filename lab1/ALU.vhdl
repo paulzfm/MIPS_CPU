7965f493b85daf134b92292e82dd2e5e25c53e7e
paulzfm <zhufengminpaul@163.com>
2015-10-14 18:43:49 +0800

    to be tested: ALU

--- lab1/ALU.vhdl
+++ lab1/ALU.vhdl
@@ -1,7 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.std_logic_arith.all;
-use ieee.std_logic_unsigned.all;
+USE ieee.numeric_std.ALL;
 
 entity ALU is 
     port (
@@ -11,3 +10,30 @@ entity ALU is
     );
 end ALU;
 
+architecture beh of ALU is
+begin
+    case op is
+        when "0000" => -- ADD
+            y <= a + b;
+        when "0001" => -- SUB
+            y <= a - b;
+        when "0010" => -- AND
+            y <= a and b;
+        when "0011" => -- OR
+            y <= a or b;
+        when "0100" => -- XOR
+            y <= a xor b;
+        when "0101" => -- NOT
+            y <= not a;
+        when "0110" => -- SLL
+            y <= a sll to_integer(unsigned(b));
+        when "0111" => -- SRL
+            y <= a srl to_integer(unsigned(b));
+        when "1000" => -- SRA
+            y <= a sra to_integer(unsigned(b));
+        when "1001" => -- ROL
+            y <= a rol to_integer(unsigned(b));
+        others => 
+            y <= (others => '0');
+    end case;
+end beh;
