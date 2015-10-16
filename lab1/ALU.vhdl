6416bbf5ae94bc27e5d5f237f0468ba3ef6708d5
qq775193759 <775193759@qq.com>
2015-10-16 09:45:16 +0800

    = err

--- lab1/ALU.vhdl
+++ lab1/ALU.vhdl
@@ -1,8 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-use IEEE.STD_LOGIC_ARITH.ALL; 
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity ALU is 
     port (
@@ -15,13 +13,48 @@ end ALU;
 
 architecture beh of ALU is
 --SIGNAL FOR PROCESS
+SIGNAL y_temp :STD_LOGIC_VECTOR (15 downto 0);
 --SIGNAL FOR PROCESS
 begin
+    process (y_temp)
+	 begin
+	     y<=y_temp;
+		      flag(3)<='1';
+				--flag(1)<='1';
+				if(y_temp = "0000000000000000") then
+				    flag(2)<='1';
+				else
+				    flag(2)<='0';
+				end if;
+--				if(a(15)='0')then
+--				    if(b(15)='0')then
+--					     if(temp_y(15)='1')then
+--						    flag(1)<='1';
+--				        end if;
+--				    end if;
+--				elsif(a(15)='1')then
+--				    if(b(15)='1')then
+--					     if(temp_y(15)='0')then
+--						    flag(1)<='1';
+--				        end if;
+--				    end if;
+--				else
+--				    flag(1)<='0';
+--				end if;
+				if((a(15 downto 15) = "0") and (b(15 downto 15) = "0") and (temp_y(15 downto 15) = "1"))then
+					flag(1) <= '1';
+				elsif((a(15 downto 15) = "1") and (b(15 downto 15) = "1") and (temp_y(15 downto 15) = "0"))then
+					flag(1) <= '1';
+				else
+					flag(1) <= '0';
+				end if;
+				flag(0 downto 0) <= y_temp(15 downto 15);
+	 end process;
     process (op,a,b)
 	 begin
     case op is
         when "0000" => -- ADD
-            y <= std_logic_vector(unsigned(a) + unsigned(b));
+            y_temp <= std_logic_vector(unsigned(a) + unsigned(b));
 --				if(temp_y(15 downto 0) = "0000000000000000")
 --					flag(2 downto 2) <= '1';
 --				else
@@ -34,41 +67,36 @@ begin
 --				else
 --					flag(1 downto 1) <= '0';
 --				end if;
-				flag(3 downto 3) <= "1";
-				flag(2 downto 2) <= "1";
-				flag(1 downto 1) <= "1";
-				flag(0 downto 0) <= "1";
         when "0001" => -- SUB
-            y <= std_logic_vector(unsigned(a) - unsigned(b));
+            y_temp <= std_logic_vector(unsigned(a) - unsigned(b));
 				--y <= a-b ;
-				flag <= "0000";
         when "0010" => -- AND
-            y <= a and b;
-				flag <= "0000";
+            y_temp <= a and b;
+				--flag() <= "0000";
         when "0011" => -- OR
-            y <= a or b;
-				flag <= "0000";
+            y_temp <= a or b;
+				--flag <= "0000";
         when "0100" => -- XOR
-            y <= a xor b;
-				flag <= "0000";
+            y_temp <= a xor b;
+				--flag <= "0000";
         when "0101" => -- NOT
-            y <= not a;
-				flag <= "0000";
+            y_temp <= not a;
+				--flag <= "0000";
         when "0110" => -- SLL
-            y <= std_logic_vector(unsigned(a) sll to_integer(unsigned(b)));
-				flag <= "0000";
+            y_temp <= std_logic_vector(unsigned(a) sll to_integer(unsigned(b)));
+				--flag <= "0000";
         when "0111" => -- SRL
-            y <= std_logic_vector(unsigned(a) srl to_integer(unsigned(b)));
-				flag <= "0000";
+            y_temp <= std_logic_vector(unsigned(a) srl to_integer(unsigned(b)));
+				--flag <= "0000";
         when "1000" => -- SRA
-            y <= to_stdlogicvector(to_bitvector(a) sra to_integer(unsigned(b)));
-				flag <= "0000";
+            y_temp <= to_stdlogicvector(to_bitvector(a) sra to_integer(unsigned(b)));
+				--flag <= "0000";
         when "1001" => -- ROL
-            y <= std_logic_vector(unsigned(a) rol to_integer(unsigned(b)));
-				flag <= "0000";
+            y_temp <= std_logic_vector(unsigned(a) rol to_integer(unsigned(b)));
+				--flag <= "0000";
         when others => 
-            y <= (others => '0');
-				flag <= "0000";
+            y_temp <= (others => '0');
+				--flag <= "0000";
     end case;
 	 end process;
 end beh;
