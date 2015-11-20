303b86a7928333244608003c07f61ce9d541e52a
nathenqian <zczyqdc@126.com>
2015-11-20 10:53:09 +0800

    finish alu and test.

--- cpu/src/ALU.vhd
+++ cpu/src/ALU.vhd
@@ -19,7 +19,8 @@
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
+use ieee.numeric_std.all;
+use ieee.std_logic_signed.all;
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 --use IEEE.NUMERIC_STD.ALL;
@@ -34,61 +35,113 @@ entity alu is
            in_data_b : in  STD_LOGIC_VECTOR (15 downto 0);
            in_op : in  STD_LOGIC_VECTOR (3 downto 0);
            out_alu_res : out  STD_LOGIC_VECTOR (15 downto 0);
-           out_alu_t : out  STD_LOGIC);
+           debug : out STD_LOGIC_VECTOR(15 downto 0));
 end alu;
 
 architecture Behavioral of alu is
 
-component add16_component is
+component add16 is
     Port ( in_data_a : in  STD_LOGIC_VECTOR (15 downto 0);
            in_data_b : in  STD_LOGIC_VECTOR (15 downto 0);
            out_output : out  STD_LOGIC_VECTOR (15 downto 0);
            out_t : out STD_LOGIC);
 end component;
 
+component sub16 is
+    Port ( in_data_a : in  STD_LOGIC_VECTOR (15 downto 0);
+           in_data_b : in  STD_LOGIC_VECTOR (15 downto 0);
+           out_output : out  STD_LOGIC_VECTOR (15 downto 0);
+           out_t : out STD_LOGIC);
+end component;
+
+
 signal add16_res, sub16_res : STD_LOGIC_VECTOR(15 downto 0);
 signal add16_t, sub16_t : STD_LOGIC;
 
 begin
-    add16 : add16_component port map(
+    add16_entity : add16 port map(
         in_data_a => in_data_a,
         in_data_b => in_data_b,
         out_output => add16_res,
         out_t => add16_t
     );
 
-    sub16 : add16_component port map(
+    sub16_entity : sub16 port map(
         in_data_a => in_data_a,
         in_data_b => in_data_b,
         out_output => sub16_res,
         out_t => sub16_t
     );
-
+    -- debug <= "1111111111111111";
+	 debug <= add16_res;
     -- two complement of input b
 
-    process (in_op, in_data_a, in_data_b)
+    process (in_op, in_data_a, in_data_b, add16_res, sub16_res, add16_t, sub16_t)
+    variable all_zero : STD_LOGIC;
     begin
         case in_op is
-            when "0000" => -- ALU add
+            when "0000" => 
+                -- ALU add 
+                -- ATTENTION! unsigned!
                 out_alu_res <= add16_res;
-                out_alu_t <= add16_t;
-            when "0001" => -- ALU sub data_a - data_b = data_a + two_complement(data_b)
+            when "0001" => 
+                -- ALU sub
+                -- ATTENTION! unsigned!
+                -- out_alu_res <= sub16_res;
                 out_alu_res <= sub16_res;
-                out_alu_t <= sub16_t;
-            when "0010" => -- ALU << data_a << data_b 
-            when "0011" => -- ALU >> data_a >> data_b
-            when "0100" => -- ALU cmp data_a == data_b
-            when "1000" => -- output data_a
-                out_output <= in_data_a;
-                out_t <= '0';
-            when "1001" => -- output data_b
-                out_output <= in_data_b;
-                out_t <= '0';
+            when "0010" => 
+                -- ALU << data_a << data_b 
+                -- logic shift
+                if (in_data_b = 0)
+                then
+                    out_alu_res <= std_logic_vector(unsigned(in_data_a) sll 8);
+                else
+                    out_alu_res <= std_logic_vector(unsigned(in_data_a) sll to_integer(unsigned(in_data_b)));
+                end if;
+            when "0011" => 
+                -- ALU >> data_a >> data_b 
+                -- logic shift
+                if (in_data_b = 0)
+                    then
+                        out_alu_res <= std_logic_vector(unsigned(in_data_a) srl 8);
+                    else
+                        out_alu_res <= std_logic_vector(unsigned(in_data_a) srl to_integer(unsigned(in_data_b)));
+                    end if; 
+            when "0100" => 
+                -- ALU data_a xor in_data_b
+                out_alu_res <= in_data_a xor in_data_b;
+            when "0101" => 
+                -- ALU cmp 
+                -- data_a == data_b => 1 
+                -- data_a != data_b => 0
+                -- UNSIGNED!!
+                all_zero := '0';
+                for i in 0 to 15
+                loop
+                    all_zero := all_zero or sub16_res(i);
+                end loop;
+                out_alu_res <= "000000000000000" & not(all_zero);
+
+            when "0110" => 
+                -- ALU signed cmp 
+                -- data_a < data_b => 1  
+                -- data_a >= data_b => 0
+                out_alu_res <= "000000000000000" & ((in_data_a(15) and not(in_data_b(15))) or
+                                                  ( not(in_data_a(15) xor in_data_b(15)) and sub16_t ));
+            when "0111" => 
+                -- ALU unsigned cmp 
+                -- data_a < data_b => 1 
+                -- data_a >= data_b => 0
+                out_alu_res <= "000000000000000" & (sub16_t);
+            when "1000" => 
+                -- output data_a
+                out_alu_res <= in_data_a;
+            when "1001" => 
+                -- output data_b
+                out_alu_res <= in_data_b;
             when others =>
-                out_output <= (others => '0');
-                out_t <= '0';
+                out_alu_res <= (others => '0');
         end case;
     end process;
 
 end Behavioral;
-
---
+++ cpu/test/test_alu.vhd
@@ -0,0 +1,204 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   21:00:44 11/19/2015
+-- Design Name:   
+-- Module Name:   Y:/Documents/code/arch/finalproject/cpu/test/test_alu.vhd
+-- Project Name:  finalproject
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: alu
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY test_alu IS
+END test_alu;
+ 
+ARCHITECTURE behavior OF test_alu IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT alu
+    PORT(
+         in_data_a : IN  std_logic_vector(15 downto 0);
+         in_data_b : IN  std_logic_vector(15 downto 0);
+         in_op : IN  std_logic_vector(3 downto 0);
+         out_alu_res : OUT  std_logic_vector(15 downto 0)
+			;debug : OUT std_logic_vector(15 downto 0)
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal in_data_a : std_logic_vector(15 downto 0) := (others => '0');
+   signal in_data_b : std_logic_vector(15 downto 0) := (others => '0');
+   signal in_op : std_logic_vector(3 downto 0) := (others => '0');
+   signal clock : std_logic;
+ 	--Outputs
+   signal out_alu_res : std_logic_vector(15 downto 0);
+	signal debug : std_logic_vector(15 downto 0);
+   -- No clocks detected in port list. Replace clock below with 
+   -- appropriate port name 
+ 
+   constant clock_period : time := 10 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: alu PORT MAP (
+          in_data_a => in_data_a,
+          in_data_b => in_data_b,
+          in_op => in_op,
+          out_alu_res => out_alu_res
+			 ,debug => debug
+        );
+
+   -- Clock process definitions
+   clock_process :process
+   begin
+		clock <= '0';
+		wait for clock_period/2;
+		clock <= '1';
+		wait for clock_period/2;
+   end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      -- hold reset state for 100 ns.
+      wait for 100 ns;	
+
+      wait for clock_period*10;
+
+      -- insert stimulus here 
+      -- add
+      in_data_a <= "0000111100001111";
+      in_data_b <= "1111000011110001";
+      in_op <= "0000";
+      wait for 20 ns;
+      in_data_a <= "0000111100001111";
+      in_data_b <= "1111000011110000";
+      in_op <= "0000";
+      wait for 20 ns;
+      -- sub
+      in_data_a <= "0000111100001111";
+      in_data_b <= "1111000011110001";
+      in_op <= "0001";
+      wait for 20 ns;
+      in_data_b <= "0000111100001111";
+      in_data_b <= "1111000011110000";
+      in_op <= "0001";
+      -- <<
+      wait for 20 ns;
+      in_data_a <= "0000111100001111";
+      in_data_b <= "0000000000000010";
+      in_op <= "0010";
+      wait for 20 ns;
+      in_data_a <= "0000111100001111";
+      in_data_b <= "0000000000000000";
+      in_op <= "0010";
+      -- >>
+      wait for 20 ns;
+      in_data_a <= "0000111100001111";
+      in_data_b <= "0000000000000010";
+      in_op <= "0011";
+      wait for 20 ns;
+      in_data_a <= "0000111100001111";
+      in_data_b <= "0000000000000000";
+      in_op <= "0011";
+      -- xor
+      wait for 20 ns;
+      in_data_a <= "0000111100001111";
+      in_data_b <= "0000000000000010";
+      in_op <= "0100";
+      wait for 20 ns;
+
+      -- alu cmp
+      in_data_a <= "0000111100001111";
+      in_data_b <= "0000000000000000";
+      in_op <= "0101";
+      wait for 20 ns;
+      in_data_a <= "0000000000000010";
+      in_data_b <= "0000000000000010";
+      in_op <= "0101";
+      wait for 20 ns;
+      
+      -- alu signed cmp
+      in_data_a <= "0000111100001111";
+      in_data_b <= "1110000000000000";
+      in_op <= "0110";
+      wait for 20 ns;
+      in_data_a <= "0000000000000010";
+      in_data_b <= "0000000000000011";
+      in_op <= "0110";
+      wait for 20 ns;
+      in_data_a <= "0000000000000011";
+      in_data_b <= "0000000000000010";
+      in_op <= "0110";
+      wait for 20 ns;
+      in_data_a <= "1000000000000010";
+      in_data_b <= "1000000000000011";
+      in_op <= "0110";
+      wait for 20 ns;
+      in_data_a <= "1000000000000011";
+      in_data_b <= "1000000000000010";
+      in_op <= "0110";
+      wait for 20 ns;
+      in_data_a <= "1000000000000011";
+      in_data_b <= "1000000000000011";
+      in_op <= "0110";
+      wait for 20 ns;
+
+      -- alu unsigned cmp
+      in_data_a <= "0000111100001111";
+      in_data_b <= "1110000000000000";
+      in_op <= "0111";
+      wait for 20 ns;
+      in_data_a <= "1000000000000010";
+      in_data_b <= "1000000000000011";
+      in_op <= "0111";
+      wait for 20 ns;
+      in_data_a <= "1000010000000011";
+      in_data_b <= "1000000000000010";
+      in_op <= "0111";
+      wait for 20 ns;
+      in_data_a <= "1000000000000011";
+      in_data_b <= "1000000000000011";
+      in_op <= "0110";
+      wait for 20 ns;
+      -- output a
+      in_data_a <= "1000010000000011";
+      in_data_b <= "1000000000000010";
+      in_op <= "1000";
+      wait for 20 ns;
+      -- output b
+      in_data_a <= "1000010000000011";
+      in_data_b <= "1000000000000010";
+      in_op <= "1001";
+      wait for 20 ns;
+      wait;
+   end process;
+
+END;
