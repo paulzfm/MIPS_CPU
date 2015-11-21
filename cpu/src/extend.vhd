ffcb1b9ee9293218de4b077330ec2070351ca696
qq775193759 <775193759@qq.com>
2015-11-21 17:17:50 +0800

    add extend

--- cpu/src/extend.vhd
+++ cpu/src/extend.vhd
@@ -30,16 +30,62 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity extend is
-    Port ( ctl_size : in  STD_LOGIC_VECTOR (1 downto 0);
+    Port ( ctl_size : in  STD_LOGIC_VECTOR (2 downto 0);
            ctl_type : in  STD_LOGIC;
            input : in  STD_LOGIC_VECTOR (15 downto 0);
            output : out  STD_LOGIC_VECTOR (15 downto 0));
 end extend;
+-- size
+-- 000 3
+-- 001 4
+-- 010 5
+-- 011 8
+-- 100 11
+--type 0 zero ext &  1 signed ext   
 
 architecture Behavioral of extend is
-
 begin
-
+process(ctl_size,ctl_type,input)
+begin
+	case ctl_size is
+		when "000" =>
+			output(2 downto 0) <= iput(2 downto 0);
+			if(ctl_type = '0') then
+				output(15 downto 3) <= (others=>'0');
+			else
+				output(15 downto 3) <= (others=>input(2));
+			end if;
+		when "001" =>
+			output(3 downto 0) <= iput(3 downto 0);
+			if(ctl_type = '0') then
+				output(15 downto 4) <= (others=>'0');
+			else
+				output(15 downto 4) <= (others=>input(3));
+			end if;
+		when "010" =>
+			output(4 downto 0) <= iput(4 downto 0);
+			if(ctl_type = '0') then
+				output(15 downto 5) <= (others=>'0');
+			else
+				output(15 downto 5) <= (others=>input(4));
+			end if;
+		when "011" =>
+			output(7 downto 0) <= iput(7 downto 0);
+			if(ctl_type = '0') then
+				output(15 downto 8) <= (others=>'0');
+			else
+				output(15 downto 8) <= (others=>input(7));
+			end if;
+		when "100" =>
+			output(10 downto 0) <= iput(10 downto 0);
+			if(ctl_type = '0') then
+				output(15 downto 11) <= (others=>'0');
+			else
+				output(15 downto 11) <= (others=>input(10));
+			end if;
+		when others => null;
+	end case;
+end process;
 
 end Behavioral;
 
